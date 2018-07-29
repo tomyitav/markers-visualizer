03d8d11ae9605ff8509c270dced1629e4cb61c2b
Tom <tomyitav@gmail.com>
2018-07-29 21:45:23 +0300

    Switched to class component

--- src/components/cars/CarsComponent.tsx
+++ src/components/cars/CarsComponent.tsx
@@ -1,31 +1,40 @@
 import * as React from 'react';
-import {Query} from "react-apollo";
+import {ICarsProps} from "./types/ICarsProps";
 import gql from 'graphql-tag';
+import {graphql} from "react-apollo/graphql";
+import {ChildProps} from "react-apollo/types";
 
-const carsQuery = gql`
+const CarsQL = graphql<{}, {}>(gql`
     {
         car {
             _id
             name
         }
     }
-`
+`);
+
+class CarsComponent extends React.Component<ChildProps<{}, {}>, {}> {
+
+    constructor(props: ICarsProps, context: any) {
+        super(props, context);
+    }
 
 
-const CarsComponent = () => {
-    return(<Query query={carsQuery}>
-        {
-                ({ data, loading }) => {
-                console.log('got data ', data)
-                    return(
-                        <div>
-                            <label>carssss</label>
-                        </div>
-                    )
-                }
+    public componentDidMount(): void {
+        console.log('Component will mount');
+    }
+
+    public render() {
+        if (!this.props.data) {
+            return;
         }
-        </Query>
-    )
+        console.log('logging data- ', this.props.data)
+        return (
+            <React.Fragment>
+                <h1>It works!</h1>
+            </React.Fragment>
+        )
+    };
 }
 
-export default CarsComponent;
+export default CarsQL(CarsComponent);
--- src/components/cars/types/ICarsProps.ts
+++ src/components/cars/types/ICarsProps.ts
@@ -1,3 +1,6 @@
-export interface ICarsProps {
-    data: any;
+import {ChildProps} from "react-apollo/types";
+import {Car} from "../../../model/Car";
+
+export interface ICarsProps extends ChildProps<{}, {}>{
+    cars: Car[];
 }
