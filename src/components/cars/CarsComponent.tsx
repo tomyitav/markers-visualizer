0e481dcde3a894c66b04ea41731617c34076aa3f
Tom <tomyitav@gmail.com>
2018-10-14 14:31:57 +0300

    Fetch with query instead of graphql

--- src/components/cars/CarsComponent.tsx
+++ src/components/cars/CarsComponent.tsx
@@ -1,14 +1,16 @@
 import * as React from 'react';
 import {ICarsProps} from "./types/ICarsProps";
-import {graphql} from "react-apollo/graphql";
+// import {graphql} from "react-apollo/graphql";
 import {addCars} from "../../store/actions/car-actions";
 import {connect} from "react-redux";
 import {IAppState} from "../../store/AppStore";
 import CarsList from "../cars-list/CarsList";
 import carQuery from "./car-query";
-import carSubscription from "./car-subscriptions";
+// import carSubscription from "./car-subscriptions";
+import {Query} from "react-apollo";
+import {Car} from "../../model/Car";
 
-const CarsQL = graphql<{}, {}>(carQuery);
+// const CarsQL = graphql<{}, {}>(carQuery);
 
 class CarsComponent extends React.Component<ICarsProps, {}> {
 
@@ -17,24 +19,36 @@ class CarsComponent extends React.Component<ICarsProps, {}> {
         super(props, context);
     }
 
-    public componentWillReceiveProps(nextProps: Readonly<ICarsProps>, nextContext: any): void {
-        this.onDataArrival(nextProps.data);
-        this.initSubscriptions(nextProps.data);
-    }
+    // public componentWillReceiveProps(nextProps: Readonly<ICarsProps>, nextContext: any): void {
+    //     this.onDataArrival(nextProps.data);
+    //     this.initSubscriptions(nextProps.data);
+    // }
 
-    public onDataArrival = (data: any) => {
-        if(data && !data.loading && data.car) {
-            this.props.onDataArrival(data.car);
-        }
+    public onDataArrival = (car: Car[]) => {
+        // if(data && !data.loading && data.car) {
+        //     this.props.onDataArrival(data.car);
+        // }
+        this.props.onDataArrival(car);
     }
 
     public render() {
-        if (!this.props.cars) {
-            return;
-        }
-
-        return (
-            <CarsList cars={this.props.cars}/>
+        // if (!this.props.cars) {
+        //     return;
+        // }
+        //
+        // return (
+        //     <CarsList cars={this.props.cars}/>
+        // )
+        return (<Query query={carQuery}>
+                {({ data: { car }, loading }) => {
+                    if(!loading) {
+                        this.onDataArrival(car)
+                    }
+                    return (
+                        <CarsList cars={this.props.cars}/>
+                    );
+                }}
+            </Query>
         )
     };
 
@@ -43,22 +57,22 @@ class CarsComponent extends React.Component<ICarsProps, {}> {
         this.unsubscribe();
     }
 
-    private initSubscriptions = (data: any) => {
-        if(data && !data.loading) {
-            // Check for existing subscription
-            if (this.unsubscribe) {
-                // Check if props have changed and, if necessary, stop the subscription
-                return;
-            }
-
-            // Subscribe
-            console.log('init subscriptions...');
-            this.unsubscribe = data.subscribeToMore({
-                document: carSubscription,
-            });
-        }
-
-    }
+    // private initSubscriptions = (data: any) => {
+    //     if(data && !data.loading) {
+    //         // Check for existing subscription
+    //         if (this.unsubscribe) {
+    //             // Check if props have changed and, if necessary, stop the subscription
+    //             return;
+    //         }
+    //
+    //         // Subscribe
+    //         console.log('init subscriptions...');
+    //         this.unsubscribe = data.subscribeToMore({
+    //             document: carSubscription,
+    //         });
+    //     }
+    //
+    // }
 }
 
 const mapActionsToProps = {
@@ -69,4 +83,4 @@ const mapStateToProps = (state: IAppState) => ({
     cars: state.cars,
 })
 
-export default CarsQL(connect(mapStateToProps, mapActionsToProps)(CarsComponent));
+export default connect(mapStateToProps, mapActionsToProps)(CarsComponent);
