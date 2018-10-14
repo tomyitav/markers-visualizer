9eb2a479a8873c7d2ced17c75a0ceb5c0482e52b
Tom <tomyitav@gmail.com>
2018-10-15 00:36:08 +0300

    Passing options to HOC graphql

--- src/components/cars/CarsComponent.tsx
+++ src/components/cars/CarsComponent.tsx
@@ -1,16 +1,14 @@
 import * as React from 'react';
 import {ICarsProps} from "./types/ICarsProps";
-// import {graphql} from "react-apollo/graphql";
+import {graphql} from "react-apollo/graphql";
 import {addCars} from "../../store/actions/car-actions";
 import {connect} from "react-redux";
 import {IAppState} from "../../store/AppStore";
 import CarsList from "../cars-list/CarsList";
 import carQuery from "./car-query";
-// import carSubscription from "./car-subscriptions";
-import {Query} from "react-apollo";
-import {Car} from "../../model/Car";
+import carSubscription from "./car-subscriptions";
 
-// const CarsQL = graphql<{}, {}>(carQuery);
+const CarsQL = graphql<{}, {}>(carQuery, {options: {pollInterval: 1000}});
 
 class CarsComponent extends React.Component<ICarsProps, {}> {
 
@@ -19,36 +17,24 @@ class CarsComponent extends React.Component<ICarsProps, {}> {
         super(props, context);
     }
 
-    // public componentWillReceiveProps(nextProps: Readonly<ICarsProps>, nextContext: any): void {
-    //     this.onDataArrival(nextProps.data);
-    //     this.initSubscriptions(nextProps.data);
-    // }
+    public componentWillReceiveProps(nextProps: Readonly<ICarsProps>, nextContext: any): void {
+        this.onDataArrival(nextProps.data);
+        this.initSubscriptions(nextProps.data);
+    }
 
-    public onDataArrival = (car: Car[]) => {
-        // if(data && !data.loading && data.car) {
-        //     this.props.onDataArrival(data.car);
-        // }
-        this.props.onDataArrival(car);
+    public onDataArrival = (data: any) => {
+        if(data && !data.loading && data.car) {
+            this.props.onDataArrival(data.car);
+        }
     }
 
     public render() {
-        // if (!this.props.cars) {
-        //     return;
-        // }
-        //
-        // return (
-        //     <CarsList cars={this.props.cars}/>
-        // )
-        return (<Query query={carQuery}>
-                {({ data: { car }, loading }) => {
-                    if(!loading) {
-                        this.onDataArrival(car)
-                    }
-                    return (
-                        <CarsList cars={this.props.cars}/>
-                    );
-                }}
-            </Query>
+        if (!this.props.cars) {
+            return;
+        }
+
+        return (
+            <CarsList cars={this.props.cars}/>
         )
     };
 
@@ -57,22 +43,22 @@ class CarsComponent extends React.Component<ICarsProps, {}> {
         this.unsubscribe();
     }
 
-    // private initSubscriptions = (data: any) => {
-    //     if(data && !data.loading) {
-    //         // Check for existing subscription
-    //         if (this.unsubscribe) {
-    //             // Check if props have changed and, if necessary, stop the subscription
-    //             return;
-    //         }
-    //
-    //         // Subscribe
-    //         console.log('init subscriptions...');
-    //         this.unsubscribe = data.subscribeToMore({
-    //             document: carSubscription,
-    //         });
-    //     }
-    //
-    // }
+    private initSubscriptions = (data: any) => {
+        if(data && !data.loading) {
+            // Check for existing subscription
+            if (this.unsubscribe) {
+                // Check if props have changed and, if necessary, stop the subscription
+                return;
+            }
+
+            // Subscribe
+            console.log('init subscriptions...');
+            this.unsubscribe = data.subscribeToMore({
+                document: carSubscription,
+            });
+        }
+
+    }
 }
 
 const mapActionsToProps = {
@@ -83,4 +69,4 @@ const mapStateToProps = (state: IAppState) => ({
     cars: state.cars,
 })
 
-export default connect(mapStateToProps, mapActionsToProps)(CarsComponent);
+export default CarsQL(connect(mapStateToProps, mapActionsToProps)(CarsComponent));
