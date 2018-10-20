6e3d5c762bb0beb68bfaa98839fa5c5479181eb0
Tom <tomyitav@gmail.com>
2018-10-20 15:20:21 +0300

    location fields component was added

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react';
 import MapComponent from "../map/map-container/Map";
+import LocationField from "../location-field/LocationField";
 
 export const Home = () =>
     <div>
         <h1>Search copmonent</h1>
+        <LocationField/>
         <MapComponent/>
     </div>
---
+++ src/components/location-field/LocationField.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+// import {FieldProps} from 'formik';
+import Geosuggest, {Suggest} from 'react-geosuggest';
+
+class LocationField extends React.Component<{}, {}> {
+    constructor(props: any, context: any) {
+        super(props, context);
+    }
+
+    public onSuggestSelect = (place: Suggest) => {
+        console.log('Got location- ', place);
+    }
+
+    public render() {
+        // const {
+        //     field: {onChange, onBlur: _, ...field},
+        //     form: {touched, errors, setFieldValue},
+        //     ...props
+        // } = this.props;
+
+        return (
+            <Geosuggest
+                placeholder="Start typing!"
+                initialValue="Hamburg"
+                onSuggestSelect={this.onSuggestSelect}
+                location={new google.maps.LatLng(53.558572, 9.9278215)}
+                radius={20} />
+        )
+    }
+}
+
+export default LocationField
